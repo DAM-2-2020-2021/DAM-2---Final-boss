99f1fe5770f4e9de331bc9af995e28ec508c1276
Snebot <snebot@cifpfbmoll.eu>
2021-04-25 11:11:53 +0200

    added proper javadoc to util/Threaded.java

--- src/main/java/eu/cifpfbmoll/util/Threaded.java
+++ src/main/java/eu/cifpfbmoll/util/Threaded.java
@@ -1,14 +1,31 @@
 package eu.cifpfbmoll.util;
 
+/**
+ * The Threaded class encapsulates the necessary functionality to
+ * start, pause, resume, stop and join a thread.
+ *
+ * <p>The Thread is initialized and started automatically when
+ * {@link Threaded#start()} is called.</p>
+ *
+ * <p>A Class extending Threaded must implement {@link Runnable#run()}
+ * like a normal {@link Runnable} would. Additionally, Threaded has
+ * a few boolean attributes which must be used to control the thread execution:</p>
+ *
+ * <ul>
+ *     <li>run: indicates if the thread should execute <code>while (this.run) {}</code>.</li>
+ *     <li>paused: indicates if the thread should be paused <code>if (this.paused) {}</code>.</li>
+ * </ul>
+ *
+ * <p>It is up to the implementation to check for the state of these variables.</p>
+ */
 public abstract class Threaded implements Runnable {
     private Thread thread = null;
     protected volatile boolean run = false;
     protected volatile boolean paused = false;
 
     /**
-     * Start thread.
-     * If the thread is not created, it will initialize it.
-     * If the thread is already created, it will resume execution.
+     * If the thread is not created initialize it,
+     * resume execution otherwise.
      *
      * @see Threaded#resume()
      */
@@ -20,6 +37,13 @@ public abstract class Threaded implements Runnable {
         } else {
             resume();
         }
+    }
+
+    /**
+     * Pause the execution of the thread.
+     */
+    public void pause() {
+        this.paused = true;
     }
 
     /**
